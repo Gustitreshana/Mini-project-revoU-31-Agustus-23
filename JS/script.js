3032aeedb2ddf18ceba6a2eed2afcd9c41654936
Gustitreshana <gustith2105@gmail.com>
2023-08-31 20:52:06 +0700

    First commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+function redirInstagram() {
+    window.location.href = "https://www.instagram.com/gustitreshana/"
+}
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/+621234567890"
+}
